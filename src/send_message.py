f89badfe5e77ea88f1cca4839c86aa716f4fd484
AtaharNurPial <atp82232@gmail.com>
2022-03-26 10:04:34 +0600

    tweaks

--- src/send_message.py
+++ src/send_message.py
@@ -18,7 +18,7 @@ def db_response(receiverId):
     return response
 
 def sendMessage(message,connectionIds):
-    # print('connectionIds',connectionIds)
+    print('connectionIds',connectionIds)
     for connectionId in connectionIds:
         response = client.post_to_connection(
         Data=message,
@@ -50,7 +50,6 @@ def lambda_handler(event, context):
         if connection_response['Items'] is not None:
             senderId = connection_response['Items'][0]['userId']
             payload = json.dumps({'Message': message, 'senderId': senderId})
-            # for receiverConnectionId in receiverConnectionIds:
             sendMessage(message=payload,connectionIds=receiverConnectionIds)
             return{
                 'statusCode': 200,
@@ -73,85 +72,3 @@ def lambda_handler(event, context):
             'message': "User is not connected."
             })
         }
-
-
-# working code block:
-# def db_response(receiverId):
-#     response = table.scan(
-#         TableName = table_name,
-#         IndexName = index_name,
-#         FilterExpression = Key('userId').eq(receiverId)
-#     )
-#     return response
-
-# def sendMessage(message,connectionIds):
-#     for connectionId in connectionIds:
-#         response = client.post_to_connection(
-#         Data=message,
-#         ConnectionId=connectionId
-#         )
-#         return response
-
-# def lambda_handler(event, context):
-
-#     print(event)
-#     connectionId = event['requestContext']['connectionId']
-#     body = json.loads(event['body'])
-#     receiverId = body['receiverId']
-#     message = body['message']
-#     table_response = db_response(receiverId)
-#     print(table_response)
-#     if table_response['Items'] is not None:
-#         receiverConnectionIds = []
-#         for item in table_response['Items']:
-#             res = [item['connectionId']]
-#             receiverConnectionIds.extend(res)
-#             # receiverConnectionId = table_response['Items'][0]['connectionId']
-#         print(receiverConnectionIds)
-#         connection_response = table.scan(
-#             TableName = table_name,
-#             FilterExpression = Key('connectionId').eq(connectionId)
-#         )
-#         if connection_response['Items'] is not None:
-#             senderId = connection_response['Items'][0]['userId']
-#             payload = json.dumps({'Message': message, 'senderId': senderId})
-#             # for receiverConnectionId in receiverConnectionIds:
-#             sendMessage(message=payload,connectionIds=receiverConnectionIds)
-#             return{
-#                 'statusCode': 200,
-#                 'body': json.dumps({
-#                     'result': payload,
-#                     'message': "Message delivered..."
-#                 })
-#             }
-#         else:
-#             return{
-#                 'statusCode': 200,
-#                 'body': json.dumps({
-#                     'message': "Message could not be delivered..."
-#                 })
-#             }
-#     else:
-#         return{
-#             'statusCode': 200,
-#             'body': json.dumps({
-#             'message': "User is not connected."
-#             })
-#         }
-
-
-
-'''
-a_dict = [{'connectionId': 'Pe-h3dHOCYcAc6A=', 'userId': 'mahfuz'}, 
-        {'connectionId': 'Pe-hgd7ACYcAdmg=', 'userId': 'mahfuz'}, 
-        {'connectionId': 'Pe6PJd4BiYcCGmw=', 'userId': 'mahfuz'}, 
-        {'connectionId': 'Pe6PffuYCYcCJKQ=', 'userId': 'mahfuz'}]
-userIds = []
-
-for item in a_dict:
-    res = [item['userId']]
-    userIds.extend(res)
-print(userIds)
-'''
-
-
